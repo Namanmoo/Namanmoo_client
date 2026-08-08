7e3f114f5a27e600c4c4a3a0daee9460ff5b1bd1
MyeongSeok98 <myong2404@gmail.com>
2026-08-08 21:40:26 +0900

    feat: RoomContentTemplate에 안전지대 기즈모를 그린다

--- Assets/Scripts/Dungeon/RoomContentTemplate.cs
+++ Assets/Scripts/Dungeon/RoomContentTemplate.cs
@@ -20,5 +20,47 @@ namespace NaManMoo.Dungeon
 
             return positions;
         }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Rect bounds = new Rect(
+                -RoomShape.Size.x * 0.5f, -RoomShape.Size.y * 0.5f,
+                RoomShape.Size.x, RoomShape.Size.y);
+            Rect inner = Rect.MinMaxRect(
+                bounds.xMin + RoomSpawnPoints.WallInset,
+                bounds.yMin + RoomSpawnPoints.WallInset,
+                bounds.xMax - RoomSpawnPoints.WallInset,
+                bounds.yMax - RoomSpawnPoints.WallInset);
+
+            Gizmos.color = Color.yellow;
+            DrawRect(bounds);
+
+            Gizmos.color = Color.cyan;
+            DrawRect(inner);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(new Vector3(0f, bounds.yMax, 0f), RoomSpawnPoints.DoorClearance);
+            Gizmos.DrawWireSphere(new Vector3(0f, bounds.yMin, 0f), RoomSpawnPoints.DoorClearance);
+            Gizmos.DrawWireSphere(new Vector3(bounds.xMax, 0f, 0f), RoomSpawnPoints.DoorClearance);
+            Gizmos.DrawWireSphere(new Vector3(bounds.xMin, 0f, 0f), RoomSpawnPoints.DoorClearance);
+
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(Vector3.zero, RoomSpawnPoints.CentreClearance);
+        }
+
+        private static void DrawRect(Rect rect)
+        {
+            var bottomLeft = new Vector3(rect.xMin, rect.yMin, 0f);
+            var bottomRight = new Vector3(rect.xMax, rect.yMin, 0f);
+            var topRight = new Vector3(rect.xMax, rect.yMax, 0f);
+            var topLeft = new Vector3(rect.xMin, rect.yMax, 0f);
+
+            Gizmos.DrawLine(bottomLeft, bottomRight);
+            Gizmos.DrawLine(bottomRight, topRight);
+            Gizmos.DrawLine(topRight, topLeft);
+            Gizmos.DrawLine(topLeft, bottomLeft);
+        }
+#endif
     }
 }
